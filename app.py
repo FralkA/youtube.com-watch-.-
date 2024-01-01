0642478cc94c419806a3fa8faf7691d3bdf8ecdc
FralkA <153741594+FralkA@users.noreply.github.com>
2024-01-01 16:58:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
 from flask import Flask, request
 import requests
 
-from flask import Flask, render_template, redirect
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    error_message = "Une erreur s'est produite"
-    retry_url = "https://www.youtube.com/@MythicalPlayer_"
-    return render_template('index.html', error_message=error_message, retry_url=retry_url)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-app = Flask(__name__)
 
 # Remplacez 'VOTRE_WEBHOOK_URL' par l'URL réel de votre webhook Discord
 WEBHOOK_URL = 'https://discord.com/api/webhooks/1189581246829957180/aRFpdCSf9AgfhrzGP0F5cjWxu1lG5-hlejBf3se-7BISIbx4VRS6aD-jiiy4BwpHgqZo'
